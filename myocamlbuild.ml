92e88df00d0f07493db2f5ea966204b3cc9f56ca
Daniel Bünzli <daniel.buenzli@erratique.ch>
2016-10-31 21:27:08 +0100

    Fix build in presence of TOPKG_CONF_DEBUGGER_SUPPORT.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -13,8 +13,8 @@ let cp_odig_ocamltop dir ext =
   rule (strf "cp %s to %s" src dst)
     ~prod:dst
     ~deps:[cmi; src]
+    ~insert:`top
     begin fun env _build ->
-      (* FIXME Windows *)
       Cmd (S [ A cp; A (env src); A (env dst)])
     end
 
@@ -23,6 +23,14 @@ let () =
   | After_rules ->
       cp_odig_ocamltop "byte" "cmo";
       cp_odig_ocamltop "native" "cmx";
-      cp_odig_ocamltop "native" "o"
+      cp_odig_ocamltop "native" "o";
+
+      (* When compiled with TOPKG_CONF_DEBUGGER_SUPPORT, topkg will
+         ask for odig_ocamltop.ml because of odig.mllib. We provide
+         the native-code one. The `bottom insertion and `top insertion
+         of previous rules ensure this file doesn't get used to
+         implement the cmo. *)
+      copy_rule ~insert:`bottom "copy odig_ocamltop.ml source"
+        "src/native/odig_ocamltop.ml" "src/odig_ocamltop.ml";
   | _ -> ()
   end

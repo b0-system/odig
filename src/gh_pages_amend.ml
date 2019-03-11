a814bb16b19ab4de4def7c7fdefd849d53c7de45
Daniel Bünzli <daniel.buenzli@erratique.ch>
2019-03-11 15:01:20 +0100

    gh-pages-amend: make sure --cname-file takes over.
    In case the root is written with a src that has a CNAME (#45).

--- src/gh_pages_amend.ml
+++ src/gh_pages_amend.ml
@@ -8,12 +8,13 @@ open B0_std
 
 let publish ~amend ~msg ~remote ~branch preserve_symlinks cname_file src dst =
   let follow_symlinks = not preserve_symlinks in
-  let update = B0_github.Pages.update ~follow_symlinks ~src:(Some src) dst in
-  let updates = [ update; B0_github.Pages.nojekyll ] in
-  let updates = match cname_file with
-  | None -> updates
-  | Some f -> B0_github.Pages.update ~src:(Some f) (Fpath.v "CNAME") :: updates
+  let dst_upd = B0_github.Pages.update ~follow_symlinks ~src:(Some src) dst in
+  let rupdates = [ B0_github.Pages.nojekyll; dst_upd ] in
+  let rupdates = match cname_file with
+  | None -> rupdates
+  | Some f -> B0_github.Pages.update ~src:(Some f) (Fpath.v "CNAME") :: rupdates
   in
+  let updates = List.rev rupdates in
   Result.bind (B0_vcs.get ()) @@ fun repo ->
   let force = true in
   B0_github.Pages.commit_updates repo ~amend ~force ~remote ~branch ~msg updates

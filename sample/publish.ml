85808eae8576ad7869e6b0739cf6a72b14ca4820
Daniel Bünzli <daniel.buenzli@erratique.ch>
2020-03-11 21:01:54 +0100

    Remove name and version watermarks from sources.

--- sample/publish.ml
+++ sample/publish.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2019 The odig programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 open B00_std
--- src/odig_odoc.ml
+++ src/odig_odoc.ml
@@ -1,7 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2018 The odig programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
-   %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
 open Odig_support

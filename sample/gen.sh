f5d83f26ed8c447c6c11b5b205a47758443f1f3a
Daniel Bünzli <daniel.buenzli@erratique.ch>
2021-04-19 23:03:23 +0200

    Tweak sample generation script.

--- sample/gen.sh
+++ sample/gen.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 eval $(opam env)
-opam update odoc odig
-odig odoc -v --odoc-theme=odig.default \
-     --index-title 4.11.1 --index-intro=intro.mld
+opam upgrade odoc odig
+
+OCAML_VERSION=$(opam info -f version ocaml)
+ODOC_VERSION=$(opam info -f source-hash odoc)
+ODIG_VERSION=$(opam info -f source-hash odig)
+
+sed -e "s/OCAML_VERSION/$OCAML_VERSION/g" \
+    -e "s/ODOC_VERSION/$ODOC_VERSION/g" \
+    -e "s/ODIG_VERSION/$ODIG_VERSION/g" intro.mld > intro-subst.mld
+
+odig odoc -v --odoc-theme=odig.default --index-title $OCAML_VERSION \
+     --index-intro=intro-subst.mld
+
+rm intro-subst.mld
